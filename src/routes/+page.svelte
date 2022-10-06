7d665105d0e2c54b52a63ccc5e385e1c9e3d90c5
SantiF <santiagoferroraffin@gmail.com>
2022-10-06 01:29:16 -0300

    act Bienvenida

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,44 @@
 <head>
 	<title>GestionCarreras</title>
 </head>
+
 <body>
-	<br>
-	<div class="container text-center">
-		<h1>Bienvenido al Sistema de Gestion de Carreras</h1>
-		
-		<a class="btn btn-outline-success" href="/gestion/auth">
+	<div class="bienvenida">
+		<h1 class="text-center">Bienvenido al Sistema de Gestion de Carreras</h1>
+		<a class="btn btn-primary w-100" href="/gestion/auth">
 			Login
 		</a>
 	</div>
 </body>
+
+<style>
+	*{
+	  margin: 0;
+	  padding: 0;
+	  box-sizing: border-box;
+  	}	
+  
+  	body{
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background: #e8f2fc;
+  	}
+
+  	.bienvenida{
+		width: 500px;
+		height: min-content;
+		padding: 20px;
+		border-radius: 12px;
+		background: rgb(196, 197, 219);
+	}
+
+	.bienvenida h1{
+		font-size: 36px;
+		margin-bottom: 25px;
+  	}
+</style>
 
 
 
